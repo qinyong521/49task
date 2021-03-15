456a78197d86f81136d9db74ec91183592547ed7
pesh178 <35772816+pesh178@users.noreply.github.com>
2021-03-15 12:28:07 +0800

    Update scheduler.js

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -292,13 +292,13 @@ let scheduler = {
 
             // 任务执行
             // 多个任务同时执行会导致日志记录类型错误，所以仅在tryRun模式开启多个任务并发执行
-            let concurrency = scheduler.isTryRun ? 3 : 3
+            let concurrency = scheduler.isTryRun ? 1 : 1
             let queue = new PQueue({ concurrency });
             console.info('调度任务中', '并发数', concurrency)
             for (let task of will_tasks) {
                 scheduler.updateTaskFile(task, {
                     // 限制执行时长2hours，runStopTime用于防止因意外原因导致isRunning=true的任务被中断，而未改变状态使得无法再次执行的问题
-                    runStopTime: moment().add(2, 'hours').format('YYYY-MM-DD HH:mm:ss'),
+                    runStopTime: moment().add(4, 'hours').format('YYYY-MM-DD HH:mm:ss'),
                     isRunning: true
                 })
                 queue.add(async () => {
